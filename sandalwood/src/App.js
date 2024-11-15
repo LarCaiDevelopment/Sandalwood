fdc7449fbf90602b244f13de58555a18c9ae5883
AleexCaires <tiagoaleex98@gmail.com>
2024-11-15 17:34:04 +0000

    Changes to app

--- sandalwood/src/App.js
+++ sandalwood/src/App.js
@@ -1,7 +1,19 @@
+import Footer from "./Components/Footer/Footer";
+import './Styles/global.scss'
 
 function App() {
   return (
-  <>Components go here</>
+    <>
+    <div id="root">
+    <div className="main-content">
+      {/* Your main content goes here */}
+      <h1>Welcome to My Website</h1>
+      <p>This is the main content of the page.</p>
+    </div>
+    <Footer />
+  </div>
+    
+    </>
   );
 }
 
