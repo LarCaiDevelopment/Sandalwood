f4986928829ddbd2080e6ea04c8b93dc113766da
AleexCaires <tiagoaleex98@gmail.com>
2024-11-15 17:37:28 +0000

    Changes to Footer Component

--- sandalwood/src/Components/Footer/Footer.jsx
+++ sandalwood/src/Components/Footer/Footer.jsx
@@ -1,46 +1,46 @@
 import "./Footer.scss";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
-import Logo from '../../Assets/Icons/logo.png'
+import Logo from "../../Assets/Icons/logo.png";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__content">
         <div className="footer__content_wrapper">
-            <img className="footer__logo" src={Logo} alt="Logo" />
-            <div class="footer__section">
+          <img className="footer__logo" src={Logo} alt="Logo" />
+          <div className="footer__section">
             <p className="footer__section_title">Working hours</p>
             <span className="line__style"></span>
             <ul className="footer__section_wrapper">
-                <li>Mon-Friday - 7am - 5pm</li>
-                <li>Saturday - closed</li>
-                <li>Sunday - closed</li>
+              <li>Mon-Friday - 7am - 5pm</li>
+              <li>Saturday - closed</li>
+              <li>Sunday - closed</li>
             </ul>
-            </div>
+          </div>
         </div>
 
         <div className="footer__section">
           <p className="footer__section_title">Office</p>
           <span className="line__style"></span>
-            <ul className="footer__section_wrapper">
-                <li>Waterlooville, United Kingdom</li>
-            </ul>
+          <ul className="footer__section_wrapper">
+            <li>Waterlooville, United Kingdom</li>
+          </ul>
         </div>
         <div className="footer__section icons">
-        <p className="footer__section_title">Get in touch</p>
-        <span className="line__style"></span>
-        <ul className="footer__section_wrapper">
-        <li>
-                    <a href="mailto:Sandalwoodcc@outlook.com">Sandalwoodcc@outlook.com</a>
-                </li>
-                <li>
-                    <a href="tel:+447825838552">+44 7825 838552</a>
-                </li>
-        </ul>
+          <p className="footer__section_title">Get in touch</p>
+          <span className="line__style"></span>
+          <ul className="footer__section_wrapper">
+            <li>
+              <a href="mailto:Sandalwoodcc@outlook.com">Sandalwoodcc@outlook.com</a>
+            </li>
+            <li>
+              <a href="tel:+447825838552">+44 7825 838552</a>
+            </li>
+          </ul>
           <ul className="footer__section_wrapper icon">
-            <FaFacebook/>
-            <FaInstagram/>
-            <FaTwitter/>
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
           </ul>
         </div>
         <span className="line__style__footer"></span>
