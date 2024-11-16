02f7ea4a02a6cfe0e626b18a3962eb5174ff4dcb
Benlarner <stroudwoodfitness@gmail.com>
2024-11-16 16:17:30 +0000

    inital header structured completed

--- sandalwood/src/App.js
+++ sandalwood/src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 import './Styles/global.scss'
 
 function App() {
   return (
     <>
     <div id="root">
+    <Header/>
     <div className="main-content">
       {/* Your main content goes here */}
       <h1>Welcome to My Website</h1>
