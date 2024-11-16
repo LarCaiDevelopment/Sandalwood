5c1fb3b69d71299b8d44f564bda5d0861fa5fd85
AleexCaires <tiagoaleex98@gmail.com>
2024-11-16 22:39:03 +0000

    Added initial styles and component for Contact component

---
+++ sandalwood/src/Components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import "./Contact.scss";
+
+function Contact() {
+  return (
+    <>
+    <div className="contact">
+        <div className="contact__image_wrapper"></div>
+        <div className="contact__form_wrapper">
+            <div className="contact__form">
+                <p className="contact__title">Get In touch!</p>
+            </div>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default Contact;
