c050c59aeef18a9a5a69ccaedf218b1856befcab
AleexCaires <tiagoaleex98@gmail.com>
2024-11-16 23:16:44 +0000

    Changes to Contact component & styles

--- sandalwood/src/Components/Contact/Contact.jsx
+++ sandalwood/src/Components/Contact/Contact.jsx
@@ -1,10 +1,13 @@
 import "./Contact.scss";
+import ContactImage from '../../Assets/Images/image_1.png'
 
 function Contact() {
   return (
     <>
     <div className="contact">
-        <div className="contact__image_wrapper"></div>
+        <div className="contact__image_wrapper">
+        <img className="contact__image" src={ContactImage} alt="ContactImage" />
+        </div>
         <div className="contact__form_wrapper">
             <div className="contact__form">
                 <p className="contact__title">Get In touch!</p>
