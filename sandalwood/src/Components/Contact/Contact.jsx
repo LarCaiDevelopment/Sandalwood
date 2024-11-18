4d5f75e92341e01b263ab43b2fe1fb712c844f40
AleexCaires <tiagoaleex98@gmail.com>
2024-11-18 23:29:57 +0000

    Changes to Contact

--- sandalwood/src/Components/Contact/Contact.jsx
+++ sandalwood/src/Components/Contact/Contact.jsx
@@ -1,20 +1,59 @@
 import "./Contact.scss";
-import ContactImage from '../../Assets/Images/image_1.png'
+import ContactImage from "../../Assets/Images/image_1.png";
 
 function Contact() {
   return (
-    <>
     <div className="contact">
-        <div className="contact__image_wrapper">
+      <div className="contact__image_wrapper">
         <img className="contact__image" src={ContactImage} alt="ContactImage" />
-        </div>
-        <div className="contact__form_wrapper">
-            <div className="contact__form">
-                <p className="contact__title">Get In touch!</p>
+      </div>
+      <div className="contact__form_wrapper">
+        <div className="contact__form">
+          <p className="contact__title">Get In Touch!</p>
+          <form
+            action="https://formspree.io/f/xjkvoyay"
+            method="POST"
+            className="contact__form_fields"
+          >
+            <div className="contact__form_row">
+              <div className="contact__form_group">
+                <label htmlFor="first-name">Name<span className="contact__required">*</span></label>
+                <input type="text" id="first-name" name="firstName" required />
+              </div>
+              <div className="contact__form_group">
+                <label htmlFor="last-name">Last Name<span className="contact__required">*</span></label>
+                <input type="text" id="last-name" name="lastName" required />
+              </div>
             </div>
+            <div className="contact__form_row">
+              <div className="contact__form_group">
+                <label htmlFor="email">Email<span className="contact__required">*</span></label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div className="contact__form_group">
+                <label htmlFor="phone">Phone<span className="contact__required">*</span></label>
+                <input 
+                  type="tel" 
+                  id="phone" 
+                  name="phone" 
+                  onInput={(e) => {
+                    e.target.value = e.target.value.replace(/[^0-9]/g, '');
+                  }}
+                  required 
+                />
+              </div>
+            </div>
+            <div className="contact__form_group_message">
+              <label htmlFor="message">Message<span className="contact__required">*</span></label>
+              <textarea id="message" name="message" rows="4" required></textarea>
+            </div>
+            <button type="submit" className="contact__submit_button">
+              SUBMIT <span className="contact__submit_icon">✈️</span>
+            </button>
+          </form>
         </div>
+      </div>
     </div>
-    </>
   );
 }
 
