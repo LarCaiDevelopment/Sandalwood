5a4da6764fd770c3f0c283031b2b8cede2c27af7
AleexCaires <tiagoaleex98@gmail.com>
2024-11-19 20:40:16 +0000

    Initial Changes to Reviews Section

--- sandalwood/src/Components/Reviews/Reviews.jsx
+++ sandalwood/src/Components/Reviews/Reviews.jsx
@@ -1,10 +1,10 @@
 import "./Reviews.scss";
-import ReviewBackground from "../../Assets/Images/reviewsImg.png"
+
 
 function Reviews() {
   return (
     <div className="reviews">
-
+        <p className="reviews__title">What our Customers have to say about us</p>
     </div>
   );
 }
