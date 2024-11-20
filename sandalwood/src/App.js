171651a8968692038bbea4d710fd8963af82c975
Benlarner <stroudwoodfitness@gmail.com>
2024-11-20 16:19:23 +0000

    added about us component

--- sandalwood/src/App.js
+++ sandalwood/src/App.js
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero/Hero";
 import Contact from "./Components/Contact/Contact.jsx";
 import Reviews from "./Components/Reviews/Reviews.jsx"
 import HowWeWork from "./Components/HowWeWork/HowWeWork.jsx";
+import AboutUs from "./Components/AboutUs/AboutUs.jsx";
 import './Styles/global.scss'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Header/>
     <div className="main-content">
       <Hero/>
+      <AboutUs/>
       <HowWeWork/>
       <Reviews/>
       <Contact/>
---
+++ sandalwood/src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,15 @@
+import "./AboutUs.scss";
+
+function AboutUs() {
+  return (
+    <div className="about">
+        <h2 className="about__heading">Sandalwood Carpentry</h2>
+        <p className="about__text">Built by hand, designed for life.</p>
+        <p className="about__text">Built by hand, designed for life.</p>
+        <p className="about__text">Built by hand, designed for life.</p>
+        <p className="about__text">Built by hand, designed for life.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
