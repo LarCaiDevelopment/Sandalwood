4fe3cfaab75685eb96d7962a2a02373daf13b248
AleexCaires <tiagoaleex98@gmail.com>
2024-11-24 15:00:27 +0000

    Final Header amends

--- sandalwood/src/Components/Header/Header.jsx
+++ sandalwood/src/Components/Header/Header.jsx
@@ -2,11 +2,18 @@ import "./Header.scss";
 import Logo from "../../Assets/Icons/logo.png";
 
 function Header() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // This enables smooth scrolling
+    });
+  };
   return (
+    
     <header className="header">
       <div className="header__content">
         <div className = "heder__left">
-          <img className="header__logo" src={Logo} alt="Logo" />
+          <img className="header__logo" src={Logo} alt="Logo" onClick={scrollToTop}  />
         </div>
         <div className="header__right">
           <a className = "header__num" href="tel:+447825838552">+44 7825 838552</a>
