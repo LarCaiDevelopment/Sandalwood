ba0e921ccd0344409ecc22d8e48ee9d459f6c0d0
AleexCaires <tiagoaleex98@gmail.com>
2024-11-25 22:47:00 +0000

    Header SEO changes

--- sandalwood/src/Components/Header/Header.jsx
+++ sandalwood/src/Components/Header/Header.jsx
@@ -5,22 +5,31 @@ function Header() {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // This enables smooth scrolling
+      behavior: "smooth", // This enables smooth scrolling
     });
   };
+
   return (
-    
     <header className="header">
       <div className="header__content">
-        <div className = "heder__left">
-          <img className="header__logo" src={Logo} alt="Logo" onClick={scrollToTop}  />
+        <div className="header__left">
+          <img
+            className="header__logo"
+            src={Logo}
+            alt="Company Logo - Click to scroll to top"
+            onClick={scrollToTop}
+          />
         </div>
         <div className="header__right">
-          <a className = "header__num" href="tel:+447825838552">+44 7825 838552</a>
-          <button className="header__button">Contact Us</button>
+          <a className="header__num" href="tel:+447825838552" aria-label="Call us at +44 7825 838552">
+            +44 7825 838552
+          </a>
+          <button className="header__button" aria-label="Contact us to learn more about our services">
+            Contact Us
+          </button>
         </div>
-       </div>
-       <span className="line__style"></span>
+      </div>
+      <span className="line__style" role="presentation" aria-hidden="true"></span>
     </header>
   );
 }
