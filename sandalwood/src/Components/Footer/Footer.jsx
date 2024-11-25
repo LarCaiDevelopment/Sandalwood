a45227085c12003249a9c7ec967827487223fc58
AleexCaires <tiagoaleex98@gmail.com>
2024-11-25 22:27:59 +0000

    SEO best practices

--- sandalwood/src/Components/Footer/Footer.jsx
+++ sandalwood/src/Components/Footer/Footer.jsx
@@ -9,52 +9,53 @@ function Footer() {
       behavior: "smooth",
     });
   };
+
   return (
     <footer className="footer">
-        <img className="footer__logo desktop" src={Logo} alt="Logo" onClick={scrollToTop} />
+      <img className="footer__logo desktop" src={Logo} alt="Logo" onClick={scrollToTop} />
       <div className="footer__content">
         <div className="footer__section_wrapper">
-        <div className="footer__content_wrapper">
-          <img className="footer__logo mobile" src={Logo} alt="Logo" />
-          <div className="footer__section">
-            <p className="footer__section_title">Working hours</p>
-            <span className="line__style"></span>
-            <ul className="footer__section_wrapper desktop">
-              <li>Mon-Friday - 7am - 5pm</li>
-              <li>Saturday - Closed</li>
-              <li>Sunday - Closed</li>
-            </ul>
+          <div className="footer__content_wrapper">
+            <img className="footer__logo mobile" src={Logo} alt="Logo" />
+            <div className="footer__section">
+              <p className="footer__section_title">Working hours</p>
+              <span className="line__style"></span>
+              <div className="footer__section_wrapper desktop">
+                <p>Mon-Friday - 7am - 5pm</p>
+                <p>Saturday - Closed</p>
+                <p>Sunday - Closed</p>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="footer__section">
-          <p className="footer__section_title">Office</p>
-          <span className="line__style"></span>
-          <ul className="footer__section_wrapper desktop">
-            <li>Waterlooville, United Kingdom</li>
-          </ul>
-        </div>
-        <div className="footer__section icons">
-          <p className="footer__section_title">Get in touch</p>
-          <span className="line__style"></span>
-          <ul className="footer__section_wrapper desktop">
-            <li>
-              <a href="mailto:Sandalwoodcc@outlook.com">Sandalwoodcc@outlook.com</a>
-            </li>
-            <li>
-              <a href="tel:+447825838552">+44 7825 838552</a>
-            </li>
-          </ul>
-          <ul className="footer__section_wrapper icon">
-            <FaFacebook />
-            <FaInstagram />
-            <FaTwitter />
-          </ul>
-        </div>
+          <div className="footer__section">
+            <p className="footer__section_title">Office</p>
+            <span className="line__style"></span>
+            <div className="footer__section_wrapper desktop">
+              <p>Waterlooville, United Kingdom</p>
+            </div>
+          </div>
+
+          <div className="footer__section icons">
+            <p className="footer__section_title">Get in touch</p>
+            <span className="line__style"></span>
+            <div className="footer__section_wrapper desktop">
+              <p>
+                <a href="mailto:Sandalwoodcc@outlook.com">Sandalwoodcc@outlook.com</a>
+              </p>
+              <p>
+                <a href="tel:+447825838552">+44 7825 838552</a>
+              </p>
+            </div>
+            <div className="footer__section_wrapper icon">
+              <FaFacebook />
+              <FaInstagram />
+              <FaTwitter />
+            </div>
+          </div>
         </div>
         <span className="line__style__footer"></span>
         <p className="footer__rights">LarCai © 2024. All rights reserved.</p>
-        
       </div>
     </footer>
   );
