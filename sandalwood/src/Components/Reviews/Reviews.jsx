3d4c9494895ec180e7a2145d3184feb35e5f0c25
AleexCaires <tiagoaleex98@gmail.com>
2024-11-25 23:17:35 +0000

    Installed new Packages and small changes to Reviews

--- sandalwood/src/Components/Reviews/Reviews.jsx
+++ sandalwood/src/Components/Reviews/Reviews.jsx
@@ -1,10 +1,15 @@
 import "./Reviews.scss";
+import { ReactGoogleReviews } from "react-google-reviews";
+import "react-google-reviews/dist/index.css";
 
 
 function Reviews() {
+  const featurableWidgetId = "842ncdd8-0f40-438d-9c...";
   return (
+    
     <div className="reviews">
         <p className="reviews__title">What our Customers have to say about us</p>
+        <ReactGoogleReviews layout="carousel" featurableId={featurableWidgetId} />
     </div>
   );
 }
