23907691aa97727b073fe5aa39bba714722fe79f
Benlarner <stroudwoodfitness@gmail.com>
2024-11-27 16:35:18 +0000

    copy added to about us and howwework

--- sandalwood/src/Components/AboutUs/AboutUs.jsx
+++ sandalwood/src/Components/AboutUs/AboutUs.jsx
@@ -4,10 +4,15 @@ function AboutUs() {
   return (
     <div className="about">
         <h2 className="about__heading">Sandalwood Carpentry</h2>
-        <p className="about__text">Built by hand, designed for life.</p>
-        <p className="about__text">Built by hand, designed for life.</p>
-        <p className="about__text">Built by hand, designed for life.</p>
-        <p className="about__text">Built by hand, designed for life.</p>
+        <p className="about__text">At Sandalwood Carpentry, we’re a team of skilled carpenters based in Tyne and Wear, North East (UK). We do all aspects of joinery and have over 15 years of experience working both domestically and abroad.</p>
+
+        <p className="about__text">We supply carpentry services to just about anyone, from homeowners & landlords to commercial projects – where we’re happy to work with builders, architects & project managers.</p>
+
+        <p className="about__text">We do all types of carpentry work including kitchen fitting, shelving, doors, architraves, skirting boards, dado rails, picture rails, box window seating, flooring, loft conversions, velux windows, dormer windows, stairs and general property renovation. At JPM, we can also help with stud walls, plasterboarding, commercial 2nd fix and shop fitouts, as well as decking & fencing for gardens.</p>
+
+        <p className="about__text">We hope you’ll find us creative, attentive, and willing to help you realise your plans – along with some inspiration from ourselves. You can see the latest portfolio of our work here and you can also follow us on Facebook to see what we’re up to and to see how we’ve helped our other customers.</p>
+
+        <p className="about__text">Visit our services page to find out more about what we do and how we can help you get the kitchen you’ve always dreamed of, how to make the most out of the space in your home with bespoke storage and also how we can replace the doors in your home to give it a new look.</p>
     </div>
   );
 }
--- sandalwood/src/Components/HowWeWork/HowWeWork.jsx
+++ sandalwood/src/Components/HowWeWork/HowWeWork.jsx
@@ -8,25 +8,25 @@ function HowWeWork() {
         <div className="howWework__wrapper">
             <p className="howWework__title">How we Work</p>
             <span className="line__style__work"></span>
-            <p className="howWework__description">We want to make this process as simple as possible for everyone involved.</p>
+            <p className="howWework__sub">We want to make this process as simple as possible for everyone involved.</p>
             <div className="howWework__sections_wrapper">
             <div className="howWework__section">
-                    <p>Step 1</p>
-                    <p>
+                    <p className="howWework__step">Step 1</p>
+                    <p className="howWework__description">
                         Begin with an in-depth consultation to discuss your project requirements, including the desired scope of work and timeframe. Following this discussion, a detailed quotation will be provided, and a mutually agreeable start date will be confirmed.
                     </p>
                 </div>
                 <HiMiniArrowLongDown size={60} />
                 <div className="howWework__section">
-                    <p>Step 2</p>
-                    <p>
+                    <p className="howWework__step">Step 2</p>
+                    <p className="howWework__description">
                         The project commences on the agreed date. Throughout the process, flexibility is maintained to accommodate minor adjustments or design refinements as needed.
                     </p>
                 </div>
                 <HiMiniArrowLongDown size={60} />
                 <div className="howWework__section">
-                    <p>Step 3</p>
-                    <p>
+                    <p className="howWework__step">Step 3</p>
+                    <p className="howWework__description">
                         The project is completed on or before the agreed deadline, ensuring all deliverables meet your expectations and standards.
                     </p>
             </div>
