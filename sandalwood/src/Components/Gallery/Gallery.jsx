57a4863a9d9df1b6e211ff9f196bade889ad7bac
Benlarner <stroudwoodfitness@gmail.com>
2024-11-27 21:34:20 +0000

    images added to gallery

--- sandalwood/src/Components/Gallery/Gallery.jsx
+++ sandalwood/src/Components/Gallery/Gallery.jsx
@@ -2,39 +2,114 @@ import React from "react";
 import { PhotoProvider, PhotoView } from "react-photo-view";
 import "react-photo-view/dist/react-photo-view.css";
 import "./Gallery.scss";
+import bathroom from '../../Assets/Images/GalleryImages/bathroom1.JPG';
+import bathroom2 from '../../Assets/Images/GalleryImages/bathroom2.JPG';
+import bathroom3 from '../../Assets/Images/GalleryImages/bathroom3.JPG';
+import bathroom4 from '../../Assets/Images/GalleryImages/bathroom4.JPG';
+import bathroom5 from '../../Assets/Images/GalleryImages/bathroom5.JPG';
+import bathroom6 from '../../Assets/Images/GalleryImages/bathroom6.JPG';
+import bathroom7 from '../../Assets/Images/GalleryImages/bathroom7.JPG';
+import bathroom8 from '../../Assets/Images/GalleryImages/bathroom8.JPG';
+import bathroom9 from '../../Assets/Images/GalleryImages/bathroom9.JPG';
+import door from '../../Assets/Images/GalleryImages/doorway1.JPG';
+import door2 from '../../Assets/Images/GalleryImages/doorway2.JPG';
+import door3 from '../../Assets/Images/GalleryImages/doorway3.JPG';
+import door4 from '../../Assets/Images/GalleryImages/doorway4.JPG';
+import kitchen from '../../Assets/Images/GalleryImages/kitchen1.JPG';
+import kitchen9 from '../../Assets/Images/GalleryImages/kitchen9.JPG';
+import kitchen2 from '../../Assets/Images/GalleryImages/kitchen2.JPG';
+import kitchen3 from '../../Assets/Images/GalleryImages/kitchen3.JPG';
+import kitchen4 from '../../Assets/Images/GalleryImages/kitchen4.JPG';
+import kitchen5 from '../../Assets/Images/GalleryImages/kitchen5.JPG';
+import kitchen6 from '../../Assets/Images/GalleryImages/kitchen6.JPG';
+import kitchen7 from '../../Assets/Images/GalleryImages/kitchen7.JPG';
+import kitchen8 from '../../Assets/Images/GalleryImages/kitchen8.JPG';
+import living from '../../Assets/Images/GalleryImages/living1.JPG';
+import living2 from '../../Assets/Images/GalleryImages/living2.JPG';
+import living3 from '../../Assets/Images/GalleryImages/living3.JPG';
+import living4 from '../../Assets/Images/GalleryImages/living4.JPG';
+import living5 from '../../Assets/Images/GalleryImages/living5.JPG';
+import living6 from '../../Assets/Images/GalleryImages/living6.JPG';
+import living7 from '../../Assets/Images/GalleryImages/living7.JPG';
+import living8 from '../../Assets/Images/GalleryImages/living8.JPG';
+import living9 from '../../Assets/Images/GalleryImages/living9.JPG';
+import fence from '../../Assets/Images/GalleryImages/fence1.JPG';
+import fence2 from '../../Assets/Images/GalleryImages/fence2.JPG';
+import fence3 from '../../Assets/Images/GalleryImages/fence3.JPG';
+import fence4 from '../../Assets/Images/GalleryImages/fence4.JPG';
+import fence5 from '../../Assets/Images/GalleryImages/fence5.JPG';
+import fence6 from '../../Assets/Images/GalleryImages/fence6.JPG';
+import fence7 from '../../Assets/Images/GalleryImages/fence7.JPG';
+import fence8 from '../../Assets/Images/GalleryImages/fence8.JPG';
+import storage from '../../Assets/Images/GalleryImages/storage1.JPG';
+import storage2 from '../../Assets/Images/GalleryImages/storage2.JPG';
+import storage3 from '../../Assets/Images/GalleryImages/storage3.JPG';
+import storage4 from '../../Assets/Images/GalleryImages/storage4.JPG';
+import storage5 from '../../Assets/Images/GalleryImages/storage5.JPG';
+import storage6 from '../../Assets/Images/GalleryImages/storage6.JPG';
 
 function Gallery() {
   const categories = [
     {
-      thumbnail: "https://via.placeholder.com/600x400?text=Category+1",
+      thumbnail: bathroom2,
       images: [
-        "https://via.placeholder.com/600x400?text=Category1-Image1",
-        "https://via.placeholder.com/600x400?text=Category1-Image2",
-        "https://via.placeholder.com/600x400?text=Category1-Image3",
+        bathroom,
+        bathroom3,
+        bathroom4,
+        bathroom5,
+        bathroom6,
+        bathroom7,
+        bathroom8,
+        bathroom9,
+        bathroom4,
       ],
     },
     {
-      thumbnail: "https://via.placeholder.com/600x400?text=Category+2",
+      thumbnail: kitchen9,
       images: [
-        "https://via.placeholder.com/600x400?text=Category2-Image1",
-        "https://via.placeholder.com/600x400?text=Category2-Image2",
-        "https://via.placeholder.com/600x400?text=Category2-Image3",
+        kitchen,
+        kitchen2,
+        kitchen3,
+        kitchen4,
+        kitchen5,
+        kitchen6,
+        kitchen7,
+        kitchen8,
       ],
     },
     {
-      thumbnail: "https://via.placeholder.com/600x400?text=Category+3",
+      thumbnail: fence,
       images: [
-        "https://via.placeholder.com/600x400?text=Category3-Image1",
-        "https://via.placeholder.com/600x400?text=Category3-Image2",
-        "https://via.placeholder.com/600x400?text=Category3-Image3",
+        fence2,
+        fence3,
+        fence4,
+        fence5,
+        fence6,
+        fence7,
+        fence8,
       ],
     },
     {
-      thumbnail: "https://via.placeholder.com/600x400?text=Category+4",
+      thumbnail: storage3,
       images: [
-        "https://via.placeholder.com/600x400?text=Category4-Image1",
-        "https://via.placeholder.com/600x400?text=Category4-Image2",
-        "https://via.placeholder.com/600x400?text=Category4-Image3",
+        storage,
+        storage2,
+        storage4,
+        storage5,
+        storage6,
+        living,
+        living2,
+        living3,
+        living4,
+        living5,
+        living6,
+        living7,
+        living8,
+        living9,
+        door,
+        door2,
+        door3,
+        door4,
       ],
     },
   ];
