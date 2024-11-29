3c2c51b2f163f13d5e3e2fa4838f9b42b46496e7
Benlarner <stroudwoodfitness@gmail.com>
2024-11-29 11:19:17 +0000

    about us copy updated

--- sandalwood/src/Components/AboutUs/AboutUs.jsx
+++ sandalwood/src/Components/AboutUs/AboutUs.jsx
@@ -3,17 +3,28 @@ import "./AboutUs.scss";
 function AboutUs() {
   return (
     <div className="about">
-        <h2 className="about__heading">Sandalwood Carpentry</h2>
-        <p className="about__text">At Sandalwood Carpentry, we’re a team of skilled carpenters based in Tyne and Wear, North East (UK). We do all aspects of joinery and have over 15 years of experience working both domestically and abroad.</p>
+  <h2 className="about__heading">Sandalwood Carpentry</h2>
+  <p className="about__text">
+    At Sandalwood Carpentry, we take pride in offering high-quality carpentry services to the South Coast of the UK.
+  </p>
 
-        <p className="about__text">We supply carpentry services to just about anyone, from homeowners & landlords to commercial projects – where we’re happy to work with builders, architects & project managers.</p>
+  <p className="about__text">
+    Our services cater to a wide range of clients, from homeowners and landlords to commercial projects. Whether you're collaborating with builders, architects, or project managers, we’re here to bring your vision to life.
+  </p>
 
-        <p className="about__text">We do all types of carpentry work including kitchen fitting, shelving, doors, architraves, skirting boards, dado rails, picture rails, box window seating, flooring, loft conversions, velux windows, dormer windows, stairs and general property renovation. At JPM, we can also help with stud walls, plasterboarding, commercial 2nd fix and shop fitouts, as well as decking & fencing for gardens.</p>
+  <p className="about__text">
+    We handle all aspects of carpentry, including kitchen installations, custom shelving, doors, architraves, skirting boards, picture rails, window seats, flooring, loft conversions, Velux and dormer windows, staircases, and full property renovations. Additionally, we specialize in stud walls, plasterboarding, commercial second-fix carpentry, shop fittings, and outdoor projects like decking and fencing to transform your garden space.
+  </p>
 
-        <p className="about__text">We hope you’ll find us creative, attentive, and willing to help you realise your plans – along with some inspiration from ourselves. You can see the latest portfolio of our work here and you can also follow us on Facebook to see what we’re up to and to see how we’ve helped our other customers.</p>
+  <p className="about__text">
+    We strive to be creative, attentive, and committed to helping you achieve your goals, offering our expertise and inspiration along the way. Browse our portfolio to see examples of our recent work, and be sure to follow us on Facebook for updates and ideas inspired by our happy customers.
+  </p>
 
-        <p className="about__text">Visit our services page to find out more about what we do and how we can help you get the kitchen you’ve always dreamed of, how to make the most out of the space in your home with bespoke storage and also how we can replace the doors in your home to give it a new look.</p>
-    </div>
+  <p className="about__text">
+    Visit our services page to learn more about how we can help you create the perfect kitchen, maximize your home’s storage with bespoke designs, or update your interior with stylish new doors. Let us help bring your ideas to life!
+  </p>
+</div>
+
   );
 }
 
